9cc442449dd05a808c94115cda47fb82382bc85b
manuela medina <manu573@live.com>
2023-04-17 23:23:31 -0400

    code

---
+++ utils/create.js
@@ -0,0 +1,17 @@
+
+const { Employee } = require("../models/employees");
+
+const createEmployeesAndManagers = async (employees, managers) => {
+    const createdManagers = await Employee.bulkCreate(managers);
+  
+    const employeesWithManagerIds = employees.map((employee, index) => {
+      return {
+        ...employee,
+        ManagerId: createdManagers[index].id,
+      };
+    });
+  
+    await Employee.bulkCreate(employeesWithManagerIds);
+  };
+  
+  module.exports = {createEmployeesAndManagers};
